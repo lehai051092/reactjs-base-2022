e313f0c45bd439431f90b19d4147b8ac2fea795f
Le Hai <le.thanh.hai@sun-asterisk.vn>
2022-11-30 09:48:46 +0700

    React Routing - Browser Router, Link, Navlink, Route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import TodoFeature from "./features/Todo";
 import AlbumFeature from "./features/Album";
 import CounterFeature from "./features/Counter";
@@ -6,23 +5,33 @@ import ColorBox from "./features/ColorBox";
 import PostFeature from "./features/Post";
 import ClockFeature from "./features/Clock";
 import MagicBoxFeature from "./features/MagicBox";
+import {NavLink, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <ClockFeature/>
-      <hr/>
-      <MagicBoxFeature/>
-      <hr/>
-      <PostFeature/>
-      <hr/>
-      <TodoFeature/>
-      <hr/>
-      <AlbumFeature/>
-      <hr/>
-      <CounterFeature/>
-      <hr/>
-      <ColorBox/>
+      <div>
+        Header
+        <p><NavLink to="/clock">Clock</NavLink></p>
+        <p><NavLink to="/magic-box">Magic Box</NavLink></p>
+        <p><NavLink to="/post">Post</NavLink></p>
+        <p><NavLink to="/todo-list">Todo List</NavLink></p>
+        <p><NavLink to="/albums">Albums</NavLink></p>
+        <p><NavLink to="/counter">Counter</NavLink></p>
+        <p><NavLink to="/color-box">Color Box</NavLink></p>
+      </div>
+
+      <Route path="/clock" component={ClockFeature}/>
+      <Route path="/magic-box" component={MagicBoxFeature}/>
+      <Route path="/post" component={PostFeature}/>
+      <Route path="/todo-list" component={TodoFeature}/>
+      <Route path="/albums" component={AlbumFeature}/>
+      <Route path="/counter" component={CounterFeature}/>
+      <Route path="/color-box" component={ColorBox}/>
+
+      <div>
+        Footer
+      </div>
     </div>
   );
 }
